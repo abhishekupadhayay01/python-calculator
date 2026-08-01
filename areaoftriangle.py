1ac3bd9c4561c0832176d8ac0c7e37566f170d62
abhishek <abhishekupadhayay60@gmail.com>
2026-08-01 15:37:22 +0530

    remove unused files

--- areaoftriangle.py
+++
@@ -1,6 +0,0 @@
-height = float(input("enter the height of triangle:"))
-base = float(input("enter thr base of triangle:"))
-
-area = (1/2)*base*height
-
-print('the area of triangle is', area)
--- lacture1.py
+++
@@ -1,5 +0,0 @@
-number = 100000
-str_number = str(number)
-
-print("value : ", str_number)
-print("data types :", type(str_number))
