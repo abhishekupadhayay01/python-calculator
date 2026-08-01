bfd67572af843c2c7ebcc8464e39882d4420ab93
abhishek <abhishekupadhayay60@gmail.com>
2026-08-01 14:51:33 +0530

    first calculator project

---
+++ areaoftriangle.py
@@ -0,0 +1,6 @@
+height = float(input("enter the height of triangle:"))
+base = float(input("enter thr base of triangle:"))
+
+area = (1/2)*base*height
+
+print('the area of triangle is', area)
---
+++ lacture1.py
@@ -0,0 +1,5 @@
+number = 100000
+str_number = str(number)
+
+print("value : ", str_number)
+print("data types :", type(str_number))
